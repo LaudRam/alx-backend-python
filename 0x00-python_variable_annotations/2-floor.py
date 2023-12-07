d97837ed1b6affb49a6d52677539f79b5678383d
LaudRam <laudiciaramasenya@gmail.com>
2023-12-07 10:53:35 +0200

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function takes a float number as parameter """
+import math
+
+
+def floor(n: float) -> int:
+    """ Accepts a floating number as parameter """
+    return math.floor(n)
